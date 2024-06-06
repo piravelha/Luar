410c27256c714a0596a5e1cdc1a1923a9ba0d6f6
Ian <piralocojacavelha@gmail.com>
2024-06-06 13:29:30 -0300

    Initial Commit

--- foo.py
+++ foo.py
@@ -1,22 +1,11 @@
+from collections import deque
 
-array = [[x * y for x in range(99, 999)] for y in range(99, 999)]
+stack = deque()
+append = stack.append
+pop = stack.pop
 
-def is_palindrome(number):
-    original_number = number
-    reversed_number = 0
+for _ in range(15_000_000):
+    append(69)
+    pop()
 
-    while number > 0:
-        digit = number % 10
-        reversed_number = reversed_number * 10 + digit
-        number = number // 10
-
-    return original_number == reversed_number
-
-flattened = []
-for xs in array:
-    for x in xs:
-        flattened.append(x)
-
-filtered = list(filter(is_palindrome, flattened))
-
-print(max(filtered))
+print("Finished!")
